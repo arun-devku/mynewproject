0f1a4ac877c25cb2b795988636e64fac9f9cbf7a
arundev <arundevunni@gmail.com>
2021-02-15 12:41:20 +0530

    added 4 to 9 assgnmnts

--- newapp/urls.py
+++ newapp/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('firstassgnmnt/', views.firstassgnmnt,name='firstassgnmnt'),
     path('assgnmnt2/', views.assgnmnt2,name='assgnmnt2'),
     path('assgnmnt3/', views.assgnmnt3,name='assgnmnt3'),
-    path('bootstrap/', views.bootstrap,name='bootstrap')
+    path('bootstrap/', views.bootstrap,name='bootstrap'),
+    path('assgnmnt4/', views.assgnmnt4,name='assgnmnt4'),
+    path('assgnmnt5/', views.assgnmnt5,name='assgnmnt5'),
+    path('assgnmnt6/', views.assgnmnt6,name='assgnmnt6'),
+    path('assgnmnt7/', views.assgnmnt7,name='assgnmnt7'),
+    path('assgnmnt8/', views.assgnmnt8,name='assgnmnt8'),
+    path('assgnmnt9/', views.assgnmnt9,name='assgnmnt9'),
     ]
  
--- newapp/views.py
+++ newapp/views.py
@@ -14,7 +14,18 @@ def assgnmnt2(request):
 def assgnmnt3(request):
     return render (request, 'assgnmnt3.html') 
 def bootstrap(request):
-    return render(request, 'bootstrap.html')           
-
+    return render(request, 'bootstrap.html')  
+def assgnmnt4(request):
+    return render (request, 'assgnmnt4.html')   
+def assgnmnt5(request):
+    return render (request, 'assgnmnt5.html')        
+def assgnmnt6(request):
+    return render (request, 'assgnmnt6.html')     
+def assgnmnt7(request):
+    return render (request, 'assgnmntbookmarks.html')    
+def assgnmnt8(request):
+    return render (request, 'assgnmnttable.html')     
+def assgnmnt9(request):
+    return render (request, 'table2.html') 
 
  
